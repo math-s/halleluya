cebed83bf3c9ff2c50411cba7f2c9a2a3adfb5eb
math-s <matheus.andrade1996@gmail.com>
2020-06-11 00:59:18 -0300

    10 jun

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,12 +3,27 @@ import express from 'express';
 const routes = express.Router();
 
 routes.get('/api', (request, response) => {
-  return response.json({'hey':'hey'});
+  return response.send("Esta é minha api :)");
 });
 
-routes.get('/api/id', (request, response) => {
-  return response.json({'hey':'hey'});
+routes.get('/api/person', (request, response) => {
+
 });
 
+routes.get('/api/person/:id', (request, response) => {
+  return response.send("person " + request.params.id + "\n")
+});
+
+routes.get('/api/team', (request, response) => {
+
+});
+
+routes.get('/api/team/:id', (request, response) => {
+
+});
+
+routes.get('/api/event', (request, response) => {
+
+});
 
 export default routes;
